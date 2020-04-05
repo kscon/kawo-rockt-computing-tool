d42fa08163fa3c4f87e154e4fc5e1f9bac55cdbc
KC_PC <kevin.conrads@rwth-aachen.de>
2020-04-05 21:39:44 +0200

    Started to refactor kawo to dorm and include file type distinciton in fetchinput.py

--- fetchinput.py
+++ fetchinput.py
@@ -3,36 +3,48 @@ import pandas as pd
 
 def get_input(options):
     filename = options['datainput']
-    print(filename)
 
-    path_of_file = 'input/' + filename
+    filename_list = filename.strip().split('.')
+    filetype = filename_list[1]
 
-    dataframe = pd.read_excel(path_of_file)
-    inputdata = dataframe.set_index('team', drop=False)
+    if filetype == 'xlsx':
+        path_of_file = 'input/' + filename
 
-    teams = inputdata.index.values.tolist()
+        dataframe = pd.read_excel(path_of_file)
+        inputdata = dataframe.set_index('team', drop=False)
 
-    data = inputdata.to_dict('series')
+        teams = inputdata.index.values.tolist()
 
-    data_zimmer = data['zimmer']
-    dict_zimmer = data_zimmer.to_dict()
+        data = inputdata.to_dict('series')
 
-    data_unvertraeglichkeiten = data['unvertraeglichkeiten']
-    dict_unvertraeglichkeiten = data_unvertraeglichkeiten.to_dict()
+        data_zimmer = data['zimmer']
+        dict_zimmer = data_zimmer.to_dict()
 
-    data_vorspeise = data['vorspeise']
-    dict_vorspeise = data_vorspeise.to_dict()
+        data_unvertraeglichkeiten = data['unvertraeglichkeiten']
+        dict_unvertraeglichkeiten = data_unvertraeglichkeiten.to_dict()
 
-    data_hauptspeise = data['hauptspeise']
-    dict_hauptspeise = data_hauptspeise.to_dict()
+        data_vorspeise = data['vorspeise']
+        dict_vorspeise = data_vorspeise.to_dict()
 
-    data_nachspeise = data['nachspeise']
-    dict_nachspeise = data_nachspeise.to_dict()
+        data_hauptspeise = data['hauptspeise']
+        dict_hauptspeise = data_hauptspeise.to_dict()
+
+        data_nachspeise = data['nachspeise']
+        dict_nachspeise = data_nachspeise.to_dict()
 
     data_kawo = data['kawo']
     dict_kawo = data_kawo.to_dict()
 
-    data_email = data['email']
-    dict_email = data_email.to_dict()
+        data_email = data['email']
+        dict_email = data_email.to_dict()
 
-    return teams, dict_zimmer, dict_unvertraeglichkeiten, dict_vorspeise, dict_hauptspeise, dict_nachspeise, dict_kawo, dict_email
+    elif filetype == 'csv':
+        print('Reading csv files is not implemented yet :C')
+
+    elif filetype == 'xls':
+        print('Reading old excel files (xls) is not implemented yet :(')
+
+    speisen = ['vorspeise', 'hauptspeise', 'nachspeise']
+    dorms = ['1', '2', '3']
+
+    return teams, dict_zimmer, dict_unvertraeglichkeiten, dict_vorspeise, dict_hauptspeise, dict_nachspeise, dict_dorm, dict_email, speisen, dorms
--- main.py
+++ main.py
@@ -9,22 +9,19 @@ def main():
     # read config file
     options = readconfig.read_config()
 
-    speisen = ['vorspeise', 'hauptspeise', 'nachspeise']
-    kawos = ['1', '2', '3']
-
     # fetch input files
-    teams, zimmer, unvertraeglichkeiten, vorspeise, hauptspeise, nachspeise, kawo, email \
+    teams, zimmer, unvertraeglichkeiten, vorspeise, hauptspeise, nachspeise, dorm, email, speisen, dorms \
         = fetchinput.get_input(options)
 
     # do preprocessing
     p, kawo_bin, number_of_teams = preprocessing.preprocessing(teams, zimmer, speisen, vorspeise, hauptspeise,
-                                                               nachspeise, kawo, kawos, options)
+                                                               nachspeise, dorm, dorms, options)
 
     # solve the model
-    x, y, mc, tm, c, d = solvemodel.solve(teams, zimmer, speisen, p, kawo_bin, kawos, number_of_teams, options)
+    x, y, mc, tm, c, d = solvemodel.solve(teams, zimmer, speisen, p, kawo_bin, dorms, number_of_teams, options)
 
     # do postprocessing and visualization
-    postprocessing.postprocessing(options, teams, speisen, zimmer, kawo, unvertraeglichkeiten, email, x, y, p, mc, tm,
+    postprocessing.postprocessing(options, teams, speisen, zimmer, dorm, unvertraeglichkeiten, email, x, y, p, mc, tm,
                                   c, d)
 
 
