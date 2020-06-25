bb0701e68be2b598d180ccc118f89989bfc33a46
KC_PC <kevin.conrads@rwth-aachen.de>
2020-06-25 19:22:51 +0200

    Removed unnecessary options in config

--- readconfig.py
+++ readconfig.py
@@ -1,7 +1,7 @@
 # This file is used to read the "CONFIG.txt" file and set its options.
 
 def read_config():
-    Options_list = {'writeoutput': 1, 'datainput': '', 'visualize': 1, 'heuristic': 0, 'pdw': 0, 'madw': 1, 'mnttw': 2}
+    Options_list = {'writeoutput': 1, 'datainput': '', 'visualize': 1, 'madw': 1, 'mnttw': 2}
     print("##### OPTIONS: #####")
     try:
         with open("CONFIG.txt", 'r') as configfile:
@@ -38,29 +38,6 @@ def read_config():
                     elif line_list[1] == '1':
                         Options_list.update(visualize=1)
                         print("Visualization active")
-                    else:
-                        flag = 1
-
-                elif line_list[0] == 'fastersolution':
-                    if line_list[1] == '0':
-                        Options_list.update(heuristic=0)
-                        print("Heuristic solution finding inactive")
-                    elif line_list[1] == '1':
-                        Options_list.update(heuristic=1)
-                        print("Heuristic solution finding active")
-                    else:
-                        flag = 1
-
-                elif line_list[0] == 'preferreddishweight':
-                    if line_list[1] == '0':
-                        Options_list.update(pdw=0)
-                        print("Priority of preferred dish optimization set to 0")
-                    elif line_list[1] == '1':
-                        Options_list.update(pdw=1)
-                        print("Priority of preferred dish optimization set to 1")
-                    elif line_list[1] == '2':
-                        Options_list.update(pdw=2)
-                        print("Priority of preferred dish optimization set to 2")
                     else:
                         flag = 1
 
